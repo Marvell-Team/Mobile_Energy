e748d49e963a2394993729a5655b7512b1063211
Vũ Thành Nam <71971442+namsdai1@users.noreply.github.com>
2021-09-08 21:11:32 +0700

    Product Screen

--- src/screens/Bottom/PostScreens/style.js
+++ src/screens/Bottom/PostScreens/style.js
@@ -1,6 +1,6 @@
 import {StyleSheet, Dimensions} from 'react-native';
 import {theme} from '@theme';
-
+import {Image, Pressable, StatusBar} from 'react-native';
 const {width} = Dimensions.get('screen');
 const {height} = Dimensions.get('screen');
 export default StyleSheet.create({
@@ -10,4 +10,18 @@ export default StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  header:{
+    paddingVertical: 16,
+    paddingTop: StatusBar.currentHeight + 12,
+  },
+  badge:{
+    width:14,
+    height:14,
+    borderRadius:7,
+    backgroundColor: theme.colors.red,
+    position: 'absolute',
+    right:-3,
+    top:-5
+  },
+  
 });
