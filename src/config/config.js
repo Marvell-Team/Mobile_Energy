604f381d2e9c9d95ae55a005fceaad61cfd12d57
Vũ Thành Nam <71971442+namsdai1@users.noreply.github.com>
2021-11-03 01:47:53 +0700

    Edit Profile api

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,11 @@
 export var api = 'http://10.0.2.2:3000';
 export let useData = {
     token:null,
-    id:null
+    id:null,
+    name:null,
+    gender:null, //
+    birthday:null,
+    phone:null,
+    address:null,
+    avatar:null,
 }
--- src/redux/api/user.js
+++ src/redux/api/user.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { api, useData } from 'config/config';
-
 export async function getUserByID(id) {
     console.log(id,api,useData.token)
     return axios
@@ -26,3 +25,27 @@ export async function getUserByID(id) {
         };
       });
   }
+  export async function EditUserByID(data) {
+    return axios
+      .post(api+'/users/edit-user/'+useData.id,data,{
+        headers: { 'Authorization': `Bearer ${useData.token}` },
+      })
+      .then((response) => {
+        console.log('response catch => getUserApi => '+response.data);
+        return response.data;
+      })
+      .then((response) => {
+        return response;
+      })
+      .catch(function (error) {
+        //Đăng nhập sai
+        //Thep api nó sẽ trả về status 401 nên bay về hàm này
+        //những không có giá trị trả về
+        //nên tạo dữ liệu erro vào hàm này
+        console.log()
+        return {
+          status: -1,
+          error: 'Có lỗi xảy ra , không kết nối được tới máy chủ',
+        };
+      });
+  }  
