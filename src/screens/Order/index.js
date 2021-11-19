f9d02ab7ccc4e3b64bd3c3b5bdc3f2069bd0dcc0
NguyenHoaiBao <japanhuynh1211@gmail.com>
2021-11-19 17:16:17 +0700

    UI donhangchitiet

--- src/screens/Order/index.js
+++ src/screens/Order/index.js
@@ -2,10 +2,12 @@ import VoteScreen from './VoteScreen';
 import VoteFinishScreen from './VoteFinishScreen';
 import OrderSuccessScreen from './OrderSuccessScreen';
 import OrderLocation from './OrderLocation';
+import OrderDetail from './OrderDetail';
 
 export const orders = {
   VOTE_SCREEN: VoteScreen,
   VOTE_FINISH_SCREEN: VoteFinishScreen,
   ORDER_SUCCESS_SCREEN: OrderSuccessScreen,
   ORDERLOCATION: OrderLocation,
+  ORDERDETAIL: OrderDetail,
 };
